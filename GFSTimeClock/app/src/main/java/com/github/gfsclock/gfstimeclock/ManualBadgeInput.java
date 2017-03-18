72bde64c460c7906c0631a489de8f29f3447f8cb
Kent Sinclair <kent.sinclair@gmail.com>
2017-03-18 19:57:19 -0400

    manual input complete need to change how paring ID works so that app does not crash on different input sizes

--- GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/ManualBadgeInput.java
+++ GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/ManualBadgeInput.java
@@ -1,8 +1,13 @@
 package com.github.gfsclock.gfstimeclock;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class ManualBadgeInput extends AppCompatActivity {
@@ -13,18 +18,36 @@ public class ManualBadgeInput extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_badge_input);
+
+        final Button submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText idEditText = (EditText) findViewById(R.id.editID);
+                String id = idEditText.getText().toString();
+                if (id.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ManualBadgeInput.this);
+                    builder.setTitle(getString(R.string.id_empty));
+                    builder.setMessage(getString(R.string.id_empty_message));
+                    String positiveText = getString(android.R.string.ok);
+                    builder.setPositiveButton(positiveText, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    barcode = id;
+                    Intent optionsScreen = new Intent(ManualBadgeInput.this, ClockOptions.class);
+                    optionsScreen.putExtra("barcode", barcode);
+                    ManualBadgeInput.this.startActivity(optionsScreen);
+                }
+            }
+        });
+
     }
 
 
-//    final Button okButton = (Button) findViewById(R.id.submitButton);
-//    /**
-//     * The processScan() is called after a succesful scan is returned from the scanBadge1() method.
-//     * This passes the contents of the scanned barcode to the ClockOptions.
-//     */
-//    public void processScan(){
-//        EditText pinEditText = (EditText) findViewById(R.id.editText);
-//        Intent optionsScreen = new Intent(ManualBadgeInput.this, ClockOptions.class);
-//        optionsScreen.putExtra("barcode", barcode);
-//        ManualBadgeInput.this.startActivity(optionsScreen);
-//    }
 }
