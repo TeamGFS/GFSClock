ae3ad4c327009476b8a25cb1b6734552a8017228
Thanh Nguyen <todokeru.bluesky@gmail.com>
2017-03-19 16:16:41 -0400

    getPunches by ID

--- GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/PunchList.java
+++ GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/PunchList.java
@@ -1,8 +1,20 @@
 package com.github.gfsclock.gfstimeclock;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PunchList {
-    private String id;
+    private List<Integer> id;
     private String startDate;
     private String endDate;
+
+    public PunchList() {}
+    public PunchList(int inID, String start, String end) {
+        id = new ArrayList<Integer>();
+        id.add(inID);
+        startDate = start;
+        endDate = end;
+    };
+
 }
