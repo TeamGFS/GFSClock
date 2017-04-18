56b64d32203a9bc2efae0347713590497098f57e
Kent Sinclair <kent.sinclair@gmail.com>
2017-04-18 09:10:17 -0400

    if scanner is connected - handle input

--- GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/ManualInputActivity.java
+++ GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/ManualInputActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class ManualInputActivity extends AppCompatActivity {
@@ -20,11 +22,35 @@ public class ManualInputActivity extends AppCompatActivity {
         setContentView(R.layout.activity_manual_badge_input);
 
         final Button submit = (Button) findViewById(R.id.submitButton);
+
+        final EditText idField = (EditText) findViewById(R.id.editID);
+
+        idField.setFocusableInTouchMode(true);
+        idField.requestFocus();
+
+        idField.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                String id = idField.getText().toString();
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    barcode = Integer.parseInt(id);
+                    Intent optionsScreen = new Intent(ManualInputActivity.this, ClockOptionsActivity.class);
+                    optionsScreen.putExtra("barcode", barcode);
+                    ManualInputActivity.this.startActivity(optionsScreen);
+                    Toast.makeText(ManualInputActivity.this, idField.getText(), Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText idEditText = (EditText) findViewById(R.id.editID);
                 String id = idEditText.getText().toString();
+
                 if (id.equals("")) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(ManualInputActivity.this);
                     builder.setTitle(getString(R.string.id_empty));
